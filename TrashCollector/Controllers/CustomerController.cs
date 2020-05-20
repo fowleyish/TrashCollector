553552a42a32e2486d5221441bc4a48e376ea1ac
Fowley <alexanderfowle@gmail.com>
2020-05-20 14:48:17 -0500

    Added FirstTimeSetup action in Customer controller; built logic into Index action in Customer controller

---
+++ TrashCollector/Controllers/CustomerController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrashCollector.Data;
+using TrashCollector.Models;
+
+namespace TrashCollector.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext ctx)
+        {
+            _context = ctx;
+        }
+
+        // GET: First time setup for new customer
+        public ActionResult FirstTimeSetup()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return View(userId);
+        }
+
+        // POST: Create customer in Customers table
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult FirstTimeSetup(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = _context.Users.Where(x => x.Id == userId).SingleOrDefault();
+            var customer = _context.Customers.Where(x => x.CustomerId.ToString() == user.Id).Single();
+            if (customer == null)
+            {
+                return RedirectToAction(nameof(FirstTimeSetup));
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
